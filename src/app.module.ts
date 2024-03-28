7af8f690c518da19b00483af9bf2a14823dc66b0
wlgns1501 <wlgns1501@gmail.com>
2024-03-28 16:55:22 +0900

    fix: update typeorm transaction

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from 'ormconfig';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+
+        return addTransactionalDataSource(new DataSource(options));
+      },
+    }),
     AuthModule,
   ],
   controllers: [AppController],
